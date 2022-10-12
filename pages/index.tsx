eae098fdc9b3e02d01996c7a170354e4a6b11c8a
John Versus <johnvs.nagendra@gmail.com>
2022-10-13 00:27:13 +0530

    updated profile

--- pages/index.tsx
+++ pages/index.tsx
@@ -119,18 +119,7 @@ const Home: NextPage = () => {
         <p className={styles.errortext}>🛠Construction in Progress🏗</p>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <footer className={styles.footer}>Powered by GitHub</footer>
     </div>
   );
 };

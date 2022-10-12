fa2810a9ef5832ebf43ca42be5a609125484f7e4
JohnVersus <15834299+JohnVersus@users.noreply.github.com>
2022-10-12 18:55:04 +0000

    Deploying to gh-pages from @ JohnVersus/JohnVersus@6c765d87876981905aad37e59387e2daa5fbfbd8 🚀

---
+++ _next/static/3qQMbg5tKhSc8Z_Wa6g55/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/9efaeea829d5e2a5.css","static/chunks/pages/index-06bf047d72640ef5.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/8Sr_l_dk_V2O7rnEvGQza/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a7540d7df8ca4160.css","static/chunks/pages/index-eb7d751ad9427c3f.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

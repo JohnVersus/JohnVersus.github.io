fff71e998a15be49e87fad188d835a24fe96e1cb
JohnVersus <15834299+JohnVersus@users.noreply.github.com>
2022-10-13 06:32:13 +0000

    Deploying to gh-pages from @ JohnVersus/JohnVersus.github.io@b5baa48d228f5674f41e335c29025d105112fd33 🚀

--- _next/static/92bjUpWC3BBUcELT2CQ04/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/540a5f4e0dc87265.css","static/chunks/pages/index-5983aeeabeb10cea.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

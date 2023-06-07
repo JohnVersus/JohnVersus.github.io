13877c72ee9a94488a92150d3c006b54572d480e
John Versus <johnvs.nagendra@gmail.com>
2023-06-07 09:28:13 +0530

    fix: navbar buttons ⤴️

--- app/Navbar/Navbar.tsx
+++ app/Navbar/Navbar.tsx
@@ -2,6 +2,8 @@
 import { useEffect, useState } from 'react';
 import styles from './Navbar.module.css';
 
+const sections = ['#about', '#tools', '#projects', '#contact'];
+
 const Navbar = () => {
   const [activeSection, setActiveSection] = useState('');
 
@@ -11,28 +13,41 @@ const Navbar = () => {
     };
 
     window.addEventListener('hashchange', onHashChange);
+
+    const onScroll = () => {
+      for (const hash of sections) {
+        const sectionElement = document.querySelector(hash);
+        const rect = sectionElement?.getBoundingClientRect();
+        if (rect?.top && rect?.bottom && rect?.top <= 0 && rect?.bottom >= 0) {
+          if (window.location.hash !== hash) {
+            window.history.replaceState(null, '', hash);
+            setActiveSection(hash);
+          }
+          return;
+        }
+      }
+    };
+    window.addEventListener('scroll', onScroll);
+
     return () => {
       window.removeEventListener('hashchange', onHashChange);
+      window.removeEventListener('scroll', onScroll);
     };
   }, []);
-
-  useEffect(() => {
-    console.log(activeSection);
-  }, [activeSection]);
 
   return (
     <div className={styles.navbar}>
       <div className={activeSection === '#about' ? styles.activeDot : styles.dot}>
-        <a href="#about"></a>
+        <a href="#about">ℹ️</a>
       </div>
       <div className={activeSection === '#tools' ? styles.activeDot : styles.dot}>
-        <a href="#tools"></a>
+        <a href="#tools">🧰</a>
       </div>
       <div className={activeSection === '#projects' ? styles.activeDot : styles.dot}>
-        <a href="#projects"></a>
+        <a href="#projects">🗂️</a>
       </div>
       <div className={activeSection === '#contact' ? styles.activeDot : styles.dot}>
-        <a href="#contact"></a>
+        <a href="#contact">💬</a>
       </div>
     </div>
   );

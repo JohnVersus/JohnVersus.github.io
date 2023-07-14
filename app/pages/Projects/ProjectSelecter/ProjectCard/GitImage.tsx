c578172122cea8a9ecdec8e3a73fad4e42a88f8d
John Versus <johnvs.nagendra@gmail.com>
2023-07-14 22:57:46 +0530

    fix: image retry, form input color 🧑‍🔧

--- app/pages/Projects/ProjectSelecter/ProjectCard/GitImage.tsx
+++ app/pages/Projects/ProjectSelecter/ProjectCard/GitImage.tsx
@@ -1,20 +1,43 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const GitImage = ({ src = '', maxRetries = 3, ...props }) => {
+  const [currentSrc, setCurrentSrc] = useState('');
   const [errorCount, setErrorCount] = useState(0);
 
-  const handleError = () => {
-    console.log(`Error loading image at ${src}. Retry count: ${errorCount + 1}`);
-    setErrorCount((count) => count + 1);
-  };
+  useEffect(() => {
+    let cancelled = false;
+    const loadImage = async () => {
+      try {
+        const response = await fetch(src);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const blob = await response.blob();
+        if (!cancelled) {
+          setCurrentSrc(URL.createObjectURL(blob));
+        }
+      } catch (error) {
+        console.log(`Error loading image at ${src}. Retry count: ${errorCount + 1}`);
+        if (errorCount < maxRetries) {
+          setErrorCount((count) => count + 1);
+        } else {
+          console.log(`Failed to load image at ${src} after ${maxRetries} attempts.`);
+          setCurrentSrc(''); // Or set to alternative content
+        }
+      }
+    };
+    loadImage();
+    return () => {
+      cancelled = true;
+    };
+  }, [src, errorCount, maxRetries]);
 
-  if (errorCount > maxRetries) {
-    console.log(`Failed to load image at ${src} after ${maxRetries} attempts.`);
-    return null; // Or you can render an alternative content
+  if (currentSrc) {
+    // eslint-disable-next-line @next/next/no-img-element
+    return <img src={currentSrc} {...props} alt={src} />;
+  } else {
+    return <></>;
   }
-
-  // eslint-disable-next-line @next/next/no-img-element
-  return <img src={src} onError={handleError} {...props} alt={src} />;
 };
 
 export default GitImage;

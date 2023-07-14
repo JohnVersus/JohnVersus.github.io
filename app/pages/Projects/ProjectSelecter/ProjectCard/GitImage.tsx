7e21b5929504123c78906bd6331c02878a70272d
John Versus <johnvs.nagendra@gmail.com>
2023-07-14 23:43:13 +0530

    chores: revert changes 🔄

--- app/pages/Projects/ProjectSelecter/ProjectCard/GitImage.tsx
+++ app/pages/Projects/ProjectSelecter/ProjectCard/GitImage.tsx
@@ -1,64 +1,22 @@
-import { useEffect, useState, useRef } from 'react';
+import { useState } from 'react';
 
 const GitImage = ({ src = '', maxRetries = 3, ...props }) => {
-  const [currentSrc, setCurrentSrc] = useState('');
   const [errorCount, setErrorCount] = useState(0);
-  const imgRef = useRef<HTMLImageElement | null>(null);
+  const [retrySuffix, setRetrySuffix] = useState(''); // New state variable for the retry suffix
 
-  const loadImage = async () => {
-    try {
-      const response = await fetch(src);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const blob = await response.blob();
-      setCurrentSrc(URL.createObjectURL(blob));
-    } catch (error) {
-      console.log(`Error loading image at ${src}. Retry count: ${errorCount + 1}`);
-      if (errorCount < maxRetries) {
-        setErrorCount((count) => count + 1);
-      } else {
-        console.log(`Failed to load image at ${src} after ${maxRetries} attempts.`);
-        setCurrentSrc(''); // Or set to alternative content
-      }
-    }
+  const handleError = () => {
+    console.log(`Error loading image at ${src}. Retry count: ${errorCount + 1}`);
+    setErrorCount((count) => count + 1);
+    setRetrySuffix(`?retry=${errorCount + 1}`); // Change the retry suffix each time handleError runs
   };
 
-  useEffect(() => {
-    let observer: IntersectionObserver | null = null;
-
-    if (imgRef.current) {
-      observer = new IntersectionObserver(
-        (entries) => {
-          if (entries[0].isIntersecting) {
-            loadImage();
-            if (observer) {
-              observer.unobserve(imgRef.current as HTMLImageElement);
-            }
-          }
-        },
-        { threshold: 0.1 },
-      );
-      observer.observe(imgRef.current);
-    }
-
-    return () => {
-      if (observer) {
-        observer.disconnect();
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    loadImage();
-  }, [src]);
-
-  if (currentSrc) {
-    // eslint-disable-next-line @next/next/no-img-element
-    return <img ref={imgRef} src={currentSrc} {...props} alt={src} />;
-  } else {
-    return <></>;
+  if (errorCount > maxRetries) {
+    console.log(`Failed to load image at ${src} after ${maxRetries} attempts.`);
+    return null; // Or you can render an alternative content
   }
+
+  // eslint-disable-next-line @next/next/no-img-element
+  return <img src={src + retrySuffix} onError={handleError} {...props} alt={src} />; // Add the retry suffix to the src prop
 };
 
 export default GitImage;

23fb94c409f934f8d6ef0049d9eb19d662c3998b
John Versus <johnvs.nagendra@gmail.com>
2023-07-16 11:32:01 +0530

    chores: SEO 🧑‍🔧

--- app/pages/Contact/Contact.tsx
+++ app/pages/Contact/Contact.tsx
@@ -40,11 +40,11 @@ const Contact = ({ href }: { href: string }) => {
             {/* <label htmlFor="name">Name</label>
           <input type="text" id="name" name="name" required /> */}
 
-            <label htmlFor="email">Email</label>
+            {/* <label htmlFor="email">Email</label>
             <input type="email" id="email" name="email" required placeholder="Enter email" />
 
             <label htmlFor="subject">Subject</label>
-            <input type="text" id="subject" name="subject" required placeholder="Enter subject" />
+            <input type="text" id="subject" name="subject" required placeholder="Enter subject" /> */}
 
             <label htmlFor="message">Message</label>
             <textarea id="message" name="message" rows={5} required placeholder="Enter message" />
